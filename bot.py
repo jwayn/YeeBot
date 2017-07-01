70fb0b374cd71b73b6d0c02c275b8422caedb2b2
jaspric <prickettjw@gmail.com>
2017-07-01 00:50:17 -0700

    added memebucks functionality

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ import random
 review_channel_id = '328980594677776384'
 general_channel_id = '329143971693461504'
 
-conn = sqlite3.connect("yee.db")
+conn = sqlite3.connect("db/yee.db")
 cur = conn.cursor()
 
 yeebot = Bot(command_prefix="!")
@@ -23,11 +23,24 @@ async def on_ready():
     await yeebot.change_presence(game=discord.Game(name="Memes"))
 
 
-@yeebot.command()
-async def meme(*args):
-    cur.execute("SELECT * FROM links WHERE status = 'approved' ORDER BY RANDOM() LIMIT 1;")
-    link = cur.fetchone()[0]
-    return await yeebot.say(link)
+@yeebot.command(pass_context=True)
+async def meme(ctx, *args):
+    user = ctx.message.author
+
+    meme_bucks = cur.execute("SELECT meme_bucks FROM users WHERE user_id = ?", (user.id,))
+    if meme_bucks:
+        cur.execute("UPDATE users SET meme_bucks = meme_bucks - 1 WHERE user_id = ?", (user.id,))
+        cur.execute("UPDATE users SET memes_requested = memes_requested + 1 WHERE user_id = ?", (user.id,))
+        conn.commit()
+        cur.execute("SELECT * FROM links WHERE status = 'approved' ORDER BY RANDOM() LIMIT 1;")
+        link = cur.fetchone()[0]
+        return await yeebot.say(link + "\n 1 Memebuck removed from {}'s balance.".format(ctx.message.author.name))
+    else:
+        cur.execute("INSERT INTO users (user_id, username, meme_bucks) VALUES (?, ?, ?)", (user.id, user.name, 99))
+        conn.commit()
+        await yeebot.say("Bank of Memerica account created for {}!")
+        return await yeebot.say(link + "\n 1 Memebuck removed from {}'s balance.".format(ctx.message.author.name))
+
 
 
 @yeebot.command(pass_context=True)
@@ -40,7 +53,7 @@ async def addmeme(ctx, *args, member: discord.Member = None):
             cur.execute("SELECT status FROM links WHERE link = ?", (args[0],))
             check = cur.fetchone()
             if check is None:
-                cur.execute("INSERT INTO links (link, status, submitter, submitter_name) VALUES (?, 'review', ?, ?)", (args[0], member.id, member.name))
+                cur.execute("INSERT INTO links (link, status, submitter_id, submitter_name) VALUES (?, 'review', ?, ?)", (args[0], member.id, member.name))
                 conn.commit()
                 cur.execute("SELECT count(*) FROM links WHERE status = 'review'")
                 count = cur.fetchone()[0]
@@ -89,47 +102,80 @@ async def review(ctx, amount=1, member: discord.Member = None):
 async def approve(ctx, *args, member: discord.Member = None):
     if member is None:
         member = ctx.message.author
-    if args[0] not in ['1', '2', '3', '4', '5']:
-        if ctx.message.channel.id == review_channel_id:
-            cur.execute("SELECT link, submitter FROM links WHERE link = ?", (args[0],))
-            row = cur.fetchone()
-            approved_link = row[0]
-            submitter = find(lambda m: m.id == row[1], ctx.message.server.members)
-            mention = submitter.mention
-            if approved_link:
-                cur.execute("UPDATE links SET status = 'approved', reviewer = ?, reviewer_id = ? where link = ?", (member.name, member.id, approved_link))
+
+    try:
+        if args[0] not in ['1', '2', '3', '4', '5', '']:
+            if ctx.message.channel.id == review_channel_id:
+                cur.execute("SELECT link, submitter_id FROM links WHERE link = ?", (args[0],))
+                row = cur.fetchone()
+                approved_link = row[0]
+                submitter = find(lambda m: m.id == row[1], ctx.message.server.members)
+                mention = submitter.mention
+                if approved_link:
+                    try:
+                        cur.execute("SELECT meme_bucks FROM users WHERE user_id = ?", (submitter.id,))
+                        row = cur.fetchone()
+                        meme_bucks = row[0]
+                        cur.execute("UPDATE users SET meme_bucks = meme_bucks + 10 WHERE user_id = ?", (submitter.id))
+                        conn.commit()
+
+                    except TypeError:
+                        cur.execute("INSERT INTO users (user_id, username, meme_bucks) VALUES (?, ?, ?)", (submitter.id, submitter.name, 110))
+                        conn.commit()
+                        return await yeebot.say("Bank of Memerica account created for {}!")
+
+                    cur.execute("UPDATE links SET status = 'approved', reviewer_name = ?, reviewer_id = ? where link = ?", (member.name, member.id, approved_link))
+                    conn.commit()
+                    await yeebot.send_message(yeebot.get_channel(general_channel_id), '{} Your link: `{}` has been approved. You have earned 10 memebucks.'.format(mention, approved_link))
+                    return await yeebot.say("`{}` has been approved.".format(approved_link))
+            else:
+                return await yeebot.say("You do not have permission to execute this command.")
+        else:
+            limiter = int(args[0])
+            cur.execute("SELECT count(*) from links where status = 'review'")
+            total_review = int(cur.fetchone()[0])
+
+            if int(total_review) < limiter:
+                limiter = int(total_review)
+            elif limiter > 5:
+                limiter = 5
+            else:
+                pass
+
+            for x in range(0, limiter):
+                cur.execute("SELECT link, submitter_id FROM links WHERE status = 'review' LIMIT 1")
+                row = cur.fetchone()
+                approved_link = row[0]
+                submitter = find(lambda m: m.id == row[1], ctx.message.server.members)
+                user_id = cur.execute("SELECT user_id FROM users WHERE user_id = ?", (submitter.id,))
+                if user_id:
+                    cur.execute("UPDATE users SET meme_bucks = meme_bucks + 10 WHERE user_id = ?", (submitter.id,))
+                else:
+                    cur.execute("INSERT INTO users (user_id, username, meme_bucks) VALUES (?, ?, ?)", (submitter.id, submitter.name, 110))
+                    conn.commit()
+                    return await yeebot.say("Bank of Memerica account created for {}!")                
+                mention = submitter.mention
+                await yeebot.send_message(yeebot.get_channel(general_channel_id), '{} Your link: `{}` has been approved. You have earned 10 memebucks!'.format(mention, approved_link))
+                cur.execute("UPDATE links SET status = 'approved', reviewer_name = ?, reviewer_id = ? where link = ?", (member.name, member.id, approved_link))
                 conn.commit()
-                await yeebot.send_message(yeebot.get_channel(general_channel_id), '{} Your link: `{}` has been approved.'.format(mention, approved_link))
-                return await yeebot.say("`{}` has been approved.".format(approved_link))
-        else:
-            return await yeebot.say("You do not have permission to execute this command.")
-
-    else:
-        limiter = int(args[0])
-        cur.execute("SELECT count(*) from links where status = 'review'")
-        total_review = int(cur.fetchone()[0])
-
-        if int(total_review) < limiter:
-            limiter = int(total_review)
-        elif limiter > 5:
-            limiter = 5
-        else:
-            pass
-
-        for x in range(0, limiter):
-            cur.execute("SELECT link, submitter FROM links WHERE status = 'review' LIMIT 1")
+            return await yeebot.say("{} links have been approved.".format(limiter))
+    except IndexError:
+            cur.execute("SELECT link, submitter_id FROM links WHERE status = 'review' LIMIT 1")
             row = cur.fetchone()
             approved_link = row[0]
             submitter = find(lambda m: m.id == row[1], ctx.message.server.members)
-            print(row)
-            print(approved_link)
-            print(row[1])
+            user_id = cur.execute("SELECT user_id FROM users WHERE user_id = ?", (submitter.id,))
+            if user_id:
+                cur.execute("UPDATE users SET meme_bucks = meme_bucks + 10 WHERE user_id = ?", (submitter.id,))
+            else:
+                cur.execute("INSERT INTO users (user_id, username, meme_bucks) VALUES (?, ?, ?)", (submitter.id, submitter.name, 110))
+                conn.commit()
+                return await yeebot.say("Bank of Memerica account created for {}!")                
             mention = submitter.mention
-            await yeebot.send_message(yeebot.get_channel(general_channel_id), '{} Your link: `{}` has been approved.'.format(mention, approved_link))
-            cur.execute("UPDATE links SET status = 'approved', reviewer = ?, reviewer_id = ? where link = ?", (member.name, member.id, approved_link))
+            await yeebot.send_message(yeebot.get_channel(general_channel_id), '{} Your link: `{}` has been approved. You have earned 10 memebucks!'.format(mention, approved_link))
+            cur.execute("UPDATE links SET status = 'approved', reviewer_name = ?, reviewer_id = ? where link = ?", (member.name, member.id, approved_link))
             conn.commit()
-        return await yeebot.say("{} links have been approved.".format(limiter))
-
+            return await yeebot.say("1 link has been approved.")
 
 @yeebot.command(pass_context=True)
 async def reject(ctx, *args, member: discord.Member = None):
@@ -137,13 +183,13 @@ async def reject(ctx, *args, member: discord.Member = None):
         member = ctx.message.author
     if args[0] not in ['1', '2', '3', '4', '5']:
         if ctx.message.channel.id == review_channel_id:
-            cur.execute("SELECT link, submitter FROM links WHERE link = ?", (args[0],))
+            cur.execute("SELECT link, submitter_id FROM links WHERE link = ?", (args[0],))
             row = cur.fetchone()
             rejected_link = row[0]
             submitter = find(lambda m: m.id == row[1], ctx.message.server.members)
             mention = submitter.mention
             if rejected_link:
-                cur.execute("UPDATE links SET status = 'rejected', reviewer = ?, reviewer_id = ? where link = ?", (member.id, member.name, rejected_link))
+                cur.execute("UPDATE links SET status = 'rejected', reviewer_name = ?, reviewer_id = ? where link = ?", (member.id, member.name, rejected_link))
                 conn.commit()
                 await yeebot.say("{} has been rejected.".format(rejected_link))
                 return await yeebot.send_message(yeebot.get_channel(general_channel_id), '{} Your link: `{}` has been rejected.'.format(mention, rejected_link))
@@ -167,7 +213,7 @@ async def reject(ctx, *args, member: discord.Member = None):
         print("limiter = " + str(limiter))
 
         for x in range(0, limiter):
-            cur.execute("SELECT link, submitter FROM links WHERE status = 'review' LIMIT 1")
+            cur.execute("SELECT link, submitter_id FROM links WHERE status = 'review' LIMIT 1")
             row = cur.fetchone()
             approved_link = row[0]
             submitter = find(lambda m: m.id == row[1], ctx.message.server.members)
@@ -176,7 +222,7 @@ async def reject(ctx, *args, member: discord.Member = None):
             print(row[1])
             mention = submitter.mention
             await yeebot.send_message(yeebot.get_channel(general_channel_id), '{} Your link: `{}` has been rejected.'.format(mention, approved_link))
-            cur.execute("UPDATE links SET status = 'rejected', reviewer = ?, reviewer_id = ? where link = ?", (member.name, member.id, approved_link))
+            cur.execute("UPDATE links SET status = 'rejected', reviewer_name = ?, reviewer_id = ? where link = ?", (member.name, member.id, approved_link))
             conn.commit()
         return await yeebot.say("{} links have been rejected.".format(limiter))
 
@@ -291,9 +337,91 @@ async def endraffle(ctx):
         return await yeebot.say("Sorry, there isn't a raffle going on right now.")
 
 
+@yeebot.command()
+async def sputnik():
+    return await yeebot.say("http://imgur.com/HPdBflu")
+
+
 @yeebot.command(pass_context=True)
 async def dickstats(*args):
     return await yeebot.say("Is this some kind of sick joke? You want me to keep track of your guys' dicks?")
+
+
+@yeebot.command(pass_context=True)
+async def balance(ctx):
+    user_id = ctx.message.author.id
+    cur.execute("SELECT meme_bucks FROM users WHERE user_id = ?", (user_id, ))
+    row = cur.fetchone()
+    if row is None:
+        return await yeebot.say("Sorry {}, you haven't made a memebucks account. Use `!memebucks` to create one!".format(ctx.message.author.name))
+    else:
+        return await yeebot.say("{}, you have {} memebucks.".format(ctx.message.author.name, row[0]))
+
+
+@yeebot.command(pass_context=True)
+async def memebucks(ctx):
+    user_id = ctx.message.author.id
+    username = ctx.message.author.name
+    cur.execute("SELECT * FROM users WHERE user_id = ?", (user_id, ))
+    row = cur.fetchone()
+    if row is None:
+        cur.execute("INSERT INTO users (user_id, username, meme_bucks) VALUES (?, ?, 100)", (user_id, username))
+        conn.commit()
+        return await yeebot.say("Congratulations {}, and thank you for establishing an account with Bank of Memerica. 100 Memebucks have been deposited into your account. Happy meming! [̲̅$̲̅(̲̅ ͡° ͜ʖ ͡°̲̅)̲̅$̲̅]".format(ctx.message.author.name))
+    else:
+        return await yeebot.say("{}, you've already established an account! Check your balance with `!balance`.".format(ctx.message.author.name))
+
+
+@yeebot.command(pass_context=True)
+async def give(ctx, *args):
+    gifter = ctx.message.author
+    giftee = ctx.message.mentions[0]
+
+    if args:
+        print("Args = " + args[0])
+
+        try:
+            payment = int(args[0])
+
+        except ValueError as e:
+            print(e)
+            return await yeebot.say("Please use the format `!give <amount> @mention` e.g.: `!give 100 @Yee#8429`")
+
+        cur.execute("SELECT meme_bucks FROM users WHERE user_id = ?", (gifter.id,))
+        row = cur.fetchone
+
+        #if gifter doesn't have a row, deny them the transaction
+        if row is None:
+            return await yeebot.say("Sorry {}, you haven't made a memebucks account. Use `!memebucks` to create one!".format(ctx.message.author.name))
+
+        else:
+            gifter_total = int(row()[0])
+            #if the gifter doesn't have enough money to make the payment, deny them
+            if gifter_total - payment < 0:
+                return await yeebot.say("Sorry {}, you don't have enough memebucks to complete this transaction.".format(ctx.message.author.name))
+            #otherwise, make the payment
+            else:
+                gifter_total -= payment
+                cur.execute("UPDATE users SET meme_bucks = ? WHERE user_id = ?", (gifter_total, gifter.id))
+                conn.commit()
+
+        cur.execute("SELECT meme_bucks FROM users WHERE user_id = ?", (giftee.id,))
+        row = cur.fetchone
+
+        try:
+            giftee_total = int(row()[0])
+            giftee_total += payment
+            cur.execute("UPDATE users SET meme_bucks = ? WHERE user_id = ?", (giftee_total, giftee.id))
+            conn.commit()
+            return await yeebot.say("Transaction completed. {} memebucks transferred from {} to {}.".format(payment, gifter.name, giftee.name))
+        except TypeError:
+            cur.execute("INSERT INTO users (user_id, username, meme_bucks) VALUES (?, ?, ?)", (giftee.id, giftee.name, 100 + payment))
+            conn.commit()
+            return await yeebot.say("Bank of Memerica account created for {}! Transaction completed. {} memebucks transferred from {} to {}.".format(giftee.name, payment, gifter.name, giftee.name))
+
+    else:
+        return await yeebot.say("Please use the format `!give <amount> @mention` e.g.: `!give 100 @Yee#8429`")
+
 
 try:
     yeebot.run(secrets.BOT_TOKEN)
