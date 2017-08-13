821a1f3f59788966d9b1d4d190783252363fe11c
jaspric <prickettjw@gmail.com>
2017-08-13 15:32:15 -0700

    "Raffle table statements working."

--- rewrite/cogs/raffle.py
+++ rewrite/cogs/raffle.py
@@ -1,46 +1,49 @@
 from discord.ext import commands
 import discord
-import sqlite
+import sqlite3
 
 
 class Raffle:
     
     def __init__(self, yeebot):
         self.yeebot = yeebot
-        self.conn = sqlite.connect('db/yee.db')
+        self.conn = sqlite3.connect('db/yee.db')
         self.cur = self.conn.cursor()
         
-        self.cur.execute("CREATE TABLE IF NOT EXISTS raffles (raffle_id INTEGER PRIMARY KEY, initiator_name TEXT,"
-                         " initiator_id TEXT, time_started TEXT DEFAULT datetime('now'), winner_name TEXT, "
-                         "winner_id TEXT, winnings_amount INTEGER, is_live INTEGER)")
+        self.cur.execute("CREATE TABLE IF NOT EXISTS raffles (raffle_id INTEGER PRIMARY KEY, is_live INTEGER," 
+                         " initiator_id TEXT, initiator_name TEXT, time_started TIMESTAMP DEFAULT CURRENT_TIMESTAMP,"
+                         " winner_id TEXT, winner_name TEXT, winnings_amount INTEGER)")
         
         self.cur.execute('CREATE TABLE IF NOT EXISTS raffle_entries (raffle_id INTEGER REFERENCES raffles(raffle_id),'
                          ' user_id TEXT, user_name TEXT, is_winner INTEGER DEFAULT 0, UNIQUE (raffle_id, user_id))')
-       
-        self.cur.execute('SELECT raffle_id FROM raffles WHERE is_live = 1')
+        
+        # Check if there is already a live raffle in the database
+        self.cur.execute('SELECT * FROM raffles WHERE is_live = 1')
         
         fetch = self.cur.fetchone()
 
         if fetch:
             self.is_live = 1
             self.live_raffle_id = fetch[0]
-        else
+            self.initiator = fetch[1]
+            self.time_started = fetch[3]
+        else:
             self.is_live = 0
 
     @commands.group(pass_context=True, description='Start a raffle.')
     async def raffle(self, ctx):
-        if Raffle.raffle_live:
-            return await self.yeebot.say(f'There if a live raffle started by {initiator} at {start_time}. There are currently {num_entries} raffle entries.')
+        if self.is_live:
+            return await self.yeebot.say(f'There if a live raffle started by {self.initiator} at {self.time_started}. There are currently X raffle entries.')
         if ctx.invoked_subcommand is None:
             return await self.yeebot.say('What do you want to do with the raffle? `!help raffle` for more information.')
 
     @raffle.command(name='start', description='Start a raffle with `!raffle start <number>`')
     async def start(self, award_amount=None):
         if award_amount is None:
-            Raffle.raffle_live = True 
+            self.is_live = True 
             return await self.yeebot.say('Enter an award amount with `!raffle start <number>`')
         else:
-            Raffle.raffle_live = True
+            self.is_live = True
             return await self.yeebot.say('Start a raffle')
 
     @raffle.command(name='end')
