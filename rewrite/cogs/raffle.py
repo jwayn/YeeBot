237d3c805fbef3778edcd66e68b987e22441e181
jaspric <prickettjw@gmail.com>
2017-08-13 21:44:02 -0700

    Added role check to raffle commands

--- rewrite/cogs/raffle.py
+++ rewrite/cogs/raffle.py
@@ -3,6 +3,13 @@ import discord
 import sqlite3
 
 
+def is_admin(ctx):
+    roles = ctx.message.author.roles
+    if 'VERY Bad Hombres' in roles or 'Bad Hombres' in roles:
+        return True
+    return False
+
+
 class Raffle:
     
     def __init__(self, yeebot):
@@ -16,7 +23,8 @@ class Raffle:
         
         self.cur.execute('CREATE TABLE IF NOT EXISTS raffle_entries (raffle_id INTEGER REFERENCES raffles(raffle_id),'
                          ' user_id TEXT, user_name TEXT, is_winner INTEGER DEFAULT 0, UNIQUE (raffle_id, user_id))')
-        
+        self.conn.commit()
+
         # Check if there is already a live raffle in the database
         self.cur.execute('SELECT * FROM raffles WHERE is_live = 1')
         
@@ -32,20 +40,12 @@ class Raffle:
 
     @commands.group(pass_context=True, description='Start a raffle.')
     async def raffle(self, ctx):
-        if self.is_live:
-            return await self.yeebot.say(f'There if a live raffle started by {self.initiator} at {self.time_started}. There are currently X raffle entries.')
         if ctx.invoked_subcommand is None:
             return await self.yeebot.say('What do you want to do with the raffle? `!help raffle` for more information.')
 
     @raffle.command(name='start', description='Start a raffle with `!raffle start <award_amount>`')
-    async def start(self, award_amount=None):
-        if award_amount is None:
-            self.is_live = True 
-            return await self.yeebot.say('Enter an award amount with `!raffle start <award_amount>`')
-        else:
-            self.is_live = True
-            return await self.yeebot.say('Start a raffle')
-
+    async def start(self, ctx, award_amount=None):
+        if ctx.
     @raffle.command(name='end')
     async def end(self):
         return await self.yeebot.say('End a raffle')
