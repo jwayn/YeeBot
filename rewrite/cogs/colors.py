9a3a29f198c837c904920f08adb82f97d9bd6d51
jaspric <prickettjw@gmail.com>
2018-03-06 20:17:11 -0800

    Colors.py complete.

--- rewrite/cogs/colors.py
+++ rewrite/cogs/colors.py
@@ -25,7 +25,8 @@ class Colors:
         if ctx.invoked_subcommand is None:
             return await self.yeebot.say('What color do you want to change to? !help colors for more information.')
 
-    @raffle.command(name='teal')
+
+    @raffle.command(name='teal' description="Change name color to teal.")
     async def teal(self, ctx):
         #select memebucks from user
         if memebucks.check_balance(ctx.message.author.id) >= 100:
@@ -38,60 +39,103 @@ class Colors:
         else:
             self.yeebot.say('You need at least 100 memebucks to change the color of your name. Get out there and submit some memes!')
 
-    
-    @raffle.command(name='green')
+
+    @raffle.command(name='green' description="Change name color to green.")
     async def green(self, ctx):
-        #change user color to green
-        #subtract 100 memebcuks from account
-        #return message stating this
-    
+        #select memebucks from user
+        if memebucks.check_balance(ctx.message.author.id) >= 100:
+            if 'green' in ctx.message.author.roles:
+                self.yeebot.say('You are already that color, silly.')
+            else:
+                self.yeebot.replace_role(ctx.message.author, 'green')
+                memebucks.withdraw(ctx.message.author.id, 100)
+                self.yeebot.say('Your new color is green. Your new balance is {}'.format(memebucks.check_balance(ctx.message.author.id)))
+        else:
+            self.yeebot.say('You need at least 100 memebucks to change the color of your name. Get out there and submit some memes!')
 
-    @raffle.command(name='blue')
+
+    @raffle.command(name='blue' description="Change name color to blue")
     async def blue(self, ctx):
-        #change user color to green
-        #subtract 100 memebcuks from account
-        #return message stating this
+        #select memebucks from user
+        if memebucks.check_balance(ctx.message.author.id) >= 100:
+            if 'blue' in ctx.message.author.roles:
+                self.yeebot.say('You are already that color, silly.')
+            else:
+                self.yeebot.replace_role(ctx.message.author, 'blue')
+                memebucks.withdraw(ctx.message.author.id, 100)
+                self.yeebot.say('Your new color is blue. Your new balance is {}'.format(memebucks.check_balance(ctx.message.author.id)))
+        else:
+            self.yeebot.say('You need at least 100 memebucks to change the color of your name. Get out there and submit some memes!')
 
 
-    @raffle.command(name='purple')
+    @raffle.command(name='purple' description="Change name color to purple.")
     async def purple(self, ctx):
-        #change user color to green
-        #subtract 100 memebcuks from account
-        #return message stating this
+        #select memebucks from user
+        if memebucks.check_balance(ctx.message.author.id) >= 100:
+            if 'purple' in ctx.message.author.roles:
+                self.yeebot.say('You are already that color, silly.')
+            else:
+                self.yeebot.replace_role(ctx.message.author, 'purple')
+                memebucks.withdraw(ctx.message.author.id, 100)
+                self.yeebot.say('Your new color is purple. Your new balance is {}'.format(memebucks.check_balance(ctx.message.author.id)))
+        else:
+            self.yeebot.say('You need at least 100 memebucks to change the color of your name. Get out there and submit some memes!')
 
 
-    @raffle.command(name='red')
+    @raffle.command(name='red' description="Change name color to red.")
     async def red(self, ctx):
-        #change user color to green
-        #subtract 100 memebcuks from account
-        #return message stating this
+        #select memebucks from user
+        if memebucks.check_balance(ctx.message.author.id) >= 100:
+            if 'red' in ctx.message.author.roles:
+                self.yeebot.say('You are already that color, silly.')
+            else:
+                self.yeebot.replace_role(ctx.message.author, 'red')
+                memebucks.withdraw(ctx.message.author.id, 100)
+                self.yeebot.say('Your new color is red. Your new balance is {}'.format(memebucks.check_balance(ctx.message.author.id)))
+        else:
+            self.yeebot.say('You need at least 100 memebucks to change the color of your name. Get out there and submit some memes!')
 
-    @raffle.command(name='red')
-    async def red(self, ctx):
-        #change user color to green
-        #subtract 100 memebcuks from account
-        #return message stating this
 
-    
-    @raffle.command(name='yellow')
+    @raffle.command(name='yellow' description="Change name color to purple.")
     async def yellow(self, ctx):
-        #change user color to green
-        #subtract 100 memebcuks from account
-        #return message stating this
-    
+        #select memebucks from user
+        if memebucks.check_balance(ctx.message.author.id) >= 100:
+            if 'yellow' in ctx.message.author.roles:
+                self.yeebot.say('You are already that color, silly.')
+            else:
+                self.yeebot.replace_role(ctx.message.author, 'yellow')
+                memebucks.withdraw(ctx.message.author.id, 100)
+                self.yeebot.say('Your new color is yellow. Your new balance is {}'.format(memebucks.check_balance(ctx.message.author.id)))
+        else:
+            self.yeebot.say('You need at least 100 memebucks to change the color of your name. Get out there and submit some memes!')
 
-    @raffle.command(name='orange')
+
+    @raffle.command(name='orange' description="Change name color to orange.")
     async def orange(self, ctx):
-        #change user color to green
-        #subtract 100 memebcuks from account
-        #return message stating this
+        #select memebucks from user
+        if memebucks.check_balance(ctx.message.author.id) >= 100:
+            if 'orange' in ctx.message.author.roles:
+                self.yeebot.say('You are already that color, silly.')
+            else:
+                self.yeebot.replace_role(ctx.message.author, 'orange')
+                memebucks.withdraw(ctx.message.author.id, 100)
+                self.yeebot.say('Your new color is orange. Your new balance is {}'.format(memebucks.check_balance(ctx.message.author.id)))
+        else:
+            self.yeebot.say('You need at least 100 memebucks to change the color of your name. Get out there and submit some memes!')
 
 
-    @raffle.command(name='grey')
+    @raffle.command(name='grey' description="Change name color to grey.")
     async def grey(self, ctx):
-        #change user color to green
-        #subtract 100 memebcuks from account
-        #return message stating this
+        #select memebucks from user
+        if memebucks.check_balance(ctx.message.author.id) >= 100:
+            if 'grey' in ctx.message.author.roles:
+                self.yeebot.say('You are already that color, silly.')
+            else:
+                self.yeebot.replace_role(ctx.message.author, 'grey')
+                memebucks.withdraw(ctx.message.author.id, 100)
+                self.yeebot.say('Your new color is grey. Your new balance is {}'.format(memebucks.check_balance(ctx.message.author.id)))
+        else:
+            self.yeebot.say('You need at least 100 memebucks to change the color of your name. Get out there and submit some memes!')
 
 def setup(yeebot):
     yeebot.add_cog(Colors(yeebot))
